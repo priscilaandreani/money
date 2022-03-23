c0738fd9a51fe7b051eee343eb3bd7cd40b99d95
priscila andreani <priscilaandreani@gmail.com>
2022-03-23 16:16:03 -0300

    server: add seeds to initialize db

--- src/server/mirageService.ts
+++ src/server/mirageService.ts
@@ -5,11 +5,34 @@ export const fakeService = () => {
     models: {
       transaction: Model,
     },
+
+    seeds(server) {
+      server.db.loadData({
+        transactions: [
+          {
+            id: 1,
+            title: 'Exemplo de depósito',
+            type: 'deposit',
+            category: 'Exemplo',
+            amount: 600,
+            createdAt: new Date('2021-02-12 09:00:00'),
+          },
+          {
+            id: 2,
+            title: 'Exemplo de retirada',
+            type: 'withdraw',
+            category: 'Exemplo',
+            amount: 200,
+            createdAt: new Date('2021-02-12 12:00:00'),
+          },
+        ],
+      });
+    },
     routes() {
       this.namespace = 'api';
 
       this.get('/transactions', () => {
-        return this.schema.all('transactions');
+        return this.schema.all('transaction');
       });
 
       this.post('/transactions', (schema, request) => {
